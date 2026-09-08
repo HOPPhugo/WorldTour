e84c2ef8bfd7e4afcc896e0738a640d4e565ea08
Hugo Schweizer <pm51srd@eduvaud.ch>
2026-09-08 10:59:49 +0200

    feat(label adding) : Now we can add a label, next will be to personelize it and make it like a task

--- TaskManager/TaskManager/Form1.Designer.cs
+++ TaskManager/TaskManager/Form1.Designer.cs
@@ -28,12 +28,144 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            panel1 = new Panel();
+            label1 = new Label();
+            panel2 = new Panel();
+            label3 = new Label();
+            pictureBox1 = new PictureBox();
+            textBox1 = new TextBox();
+            panel3 = new Panel();
+            button1 = new Button();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.CornflowerBlue;
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(-8, 45);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(402, 87);
+            panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial Rounded MT Bold", 55F);
+            label1.Location = new Point(74, -2);
+            label1.Name = "label1";
+            label1.Size = new Size(249, 86);
+            label1.TabIndex = 1;
+            label1.Text = "Tasks";
+            label1.Click += label1_Click;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.CornflowerBlue;
+            panel2.Location = new Point(-7, 198);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(402, 587);
+            panel2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Arial Rounded MT Bold", 15F);
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(73, 789);
+            label3.Name = "label3";
+            label3.Size = new Size(239, 23);
+            label3.TabIndex = 2;
+            label3.Text = "Task Completed : 10/10";
+            label3.Click += label3_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.Image = Properties.Resources.Rounded_rectangle;
+            pictureBox1.Location = new Point(-18, 142);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(177, 50);
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.CornflowerBlue;
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Cursor = Cursors.IBeam;
+            textBox1.Font = new Font("Arial Rounded MT Bold", 15F);
+            textBox1.Location = new Point(11, 153);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "Placeholder";
+            textBox1.Size = new Size(115, 24);
+            textBox1.TabIndex = 7;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(button1);
+            panel3.Controls.Add(panel1);
+            panel3.Controls.Add(pictureBox1);
+            panel3.Controls.Add(panel2);
+            panel3.Location = new Point(0, 1);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(395, 841);
+            panel3.TabIndex = 8;
+            panel3.Click += panel3_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Transparent;
+            button1.BackgroundImage = (Image)resources.GetObject("button1.BackgroundImage");
+            button1.BackgroundImageLayout = ImageLayout.None;
+            button1.FlatAppearance.BorderColor = Color.CornflowerBlue;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Arial Rounded MT Bold", 9F);
+            button1.Location = new Point(265, 147);
+            button1.Name = "button1";
+            button1.Size = new Size(92, 40);
+            button1.TabIndex = 7;
+            button1.Text = "Add";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.HotTrack;
+            ClientSize = new Size(388, 835);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(panel3);
+            Name = "Form1";
+            Text = "Task Manager";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel3.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private Panel panel2;
+        private Label label3;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox3;
+        private TextBox textBox1;
+        private ContextMenuStrip contextMenuStrip1;
+        private Panel panel3;
+        private Button button1;
     }
 }
--- TaskManager/TaskManager/Form1.cs
+++ TaskManager/TaskManager/Form1.cs
@@ -1,10 +1,98 @@
+using System.Runtime.CompilerServices;
+using TaskObj;
 namespace TaskManager
 {
     public partial class Form1 : Form
     {
+
+        private int nbrTask = 0;
+        private int nbrCompletedTask = 0;
+        private List<Tache> tasks = new List<Tache>();
+
         public Form1()
         {
             InitializeComponent();
+            FormLoad();
+        }
+        private void FormLoad ()
+        {
+            pictureBox1.Select();
+            label3.Text = $"Task Completed: {nbrCompletedTask} / {nbrTask}";
+            foreach (Control p in panel3.Controls)
+            {
+                p.Click += (e, s) =>
+                {
+                    panel1.Select();
+                };
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            panel1.Select();
+
+        }
+
+        private void panel3_Click(object sender, EventArgs e)
+        {
+            panel1.Select();
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            label3.Select();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SuspendLayout();
+            panel1.SuspendLayout();
+            panel3.SuspendLayout();
+            tasks.Add(new Tache(textBox1.Text));
+            // Creating and setting the label 
+            Label mylab = new Label();
+            mylab.Text = "GeeksforGeeks";
+            mylab.Location = new Point(222, 90);
+            mylab.AutoSize = true;
+            mylab.Font = new Font("Calibri", 18);
+            mylab.ForeColor = Color.Green;
+            mylab.Padding = new Padding(6);
+
+            // Adding this control to the form 
+            this.Controls.Clear();
+            this.Controls.Add(mylab);
+            InitializeComponent();
+            FormLoad();
+            ResumeLayout();
+            panel1.ResumeLayout();
+            panel3.ResumeLayout();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
         }
     }
+}
+namespace TaskObj
+{
+    public class Tache
+    {
+        private int x = 50 ;
+        private const int y = 20;
+        public Label label8 { get; protected set; }
+        public Tache(string name)
+        {
+            label8 = new Label();
+            label8.Text = name;
+            label8.Location = new Point(x, y);
+            label8.AutoSize = true;
+            label8.Font = new Font("Calibri", 18);
+            label8.ForeColor = Color.Black;
+            label8.Padding = new Padding(6);
+        }
+    }
+
 }
